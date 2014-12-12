ffafc252bccabd5d5b874c8ab46e108377cccfe2
CraigCollins <chcollin@ucsc.edu>
2014-12-11 21:16:44 -0800

    Repush of friendship
    Repush

--- ScheduleHelper/controllers/default.py
+++ ScheduleHelper/controllers/default.py
@@ -183,17 +183,25 @@ def friends():
 def friendship():
     """Ajax callback!"""
     if request.env.request_method != 'POST': raise HTTP(400)
-    if a0=='request' and not Link(source=a1, target=me):
+
+    if a0=='request' and (not Link(source=me, target=a1)) and not (int(a1)==int(me)):
         # insert a new friendship request
         Link.insert(source=me, target=a1)
+        #db(Link.target==a1)(Link.source==me).update(accepted=True)
+        #Link.insert(source=a1, target=me)
     elif a0=='accept':
         # accept an existing friendship request
+        #db(Link.target==me)(Link.source==a1).update(accepted=True)
         db(Link.target==me)(Link.source==a1).update(accepted=True)
-        if not db(Link.source==me)(Link.target==a1).count():
+        if not db(Link.source==me)(Link.target==a1).count() :
+            #print("here")
             Link.insert(source=me, target=a1)
+            db(Link.target==a1)(Link.source==me).update(accepted=True)
+            #Link.insert(source=a1, target=me)
     elif a0=='deny':
         # deny an existing friendship request
         db(Link.target==me)(Link.source==a1).delete()
     elif a0=='remove':
         # delete a previous friendship request
         db(Link.source==me)(Link.target==a1).delete()
+        db(Link.source==a1)(Link.target==me).delete()
