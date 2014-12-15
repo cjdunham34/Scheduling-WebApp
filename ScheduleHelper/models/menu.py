acf9fb13b282a81038023fb79f79e7756d03e6ce
juliarielle <juliarielle@users.noreply.github.com>
2014-12-14 20:13:04 -0800

    Update menu.py

--- ScheduleHelper/models/menu.py
+++ ScheduleHelper/models/menu.py
@@ -10,10 +10,10 @@ response.subtitle = T('coordinate with friends!')
 
 ## read more at http://dev.w3.org/html5/markup/meta.name.html
 response.meta.author = 'Your Name <you@example.com>'
-response.meta.description = 'a cool new app'
-response.meta.keywords = 'web2py, python, framework'
-response.meta.generator = 'Web2py Web Framework'
-response.meta.copyright = 'Copyright 2011'
+response.meta.description = ''
+response.meta.keywords = ''
+response.meta.generator = ''
+response.meta.copyright = ''
 
 ## your http://google.com/analytics id
 response.google_analytics_id = None
@@ -24,8 +24,8 @@ response.google_analytics_id = None
 
 response.menu = [
     (T('Home'), False, URL('default', 'index')),
-    (T('Schedule'), False, URL('default', 'edit_schedule')),
-    (T('Wall'), False, URL('default', 'wall')),
+    (T('Edit Schedule'), False, URL('default', 'edit_schedule')),
+    (T('Inbox'), False, URL('default', 'wall')),
     (T('Friends'), False, URL('default', 'friends')),
     (T('Search'), False, URL('default', 'search')),
     ]
